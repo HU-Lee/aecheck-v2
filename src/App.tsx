62251eb4e61cfa3fda6b1a9414b631cadf6ad385
Hyunuk Lee <as0266@naver.com>
2022-07-28 20:07:58 +0900

    220728 JAP 2.13.81

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,34 @@ import ManifestPage from './components/pages/ManifestPage';
 import { GLO_VER, JAP_VER } from './data/config';
 
 const announceHTML = `<div class="announce">
-  글로벌판 라디아스AS 반영
+  일본판 ${JAP_VER} 반영
   <br/>
-  Update Radias(AS) to GLOBAL Server
+  Update JAP Ver.${JAP_VER}
   <br/>
   <br/>
   그 외 자세한 사항은 링크를 확인해 주세요. 
   <br/>
   Please Check following link.
   <br/>
-  <a href="https://aecheck.tistory.com/18" target="_blank" rel="noreferrer">Patch Note</a>
+  <a href="https://aecheck.tistory.com/19" target="_blank" rel="noreferrer">Patch Note</a>
 </div>`
 
 function App() {
 
-  const announceViewed = Boolean(window.localStorage.getItem("a_v_220725"))
+  const announceViewed = Boolean(window.localStorage.getItem("a_v_220728"))
 
   const { lang } = useContext(AnotherContext)
   const message = require(`./language/${lang}.json`)
 
   useEffect(() => {
-    window.localStorage.removeItem("a_v_220708")
+    window.localStorage.removeItem("a_v_220725")
     if(!announceViewed) {
       Swal.fire({
-        title: 'Update - 22.07.08',
+        title: 'Update - 22.07.28',
         html: announceHTML,
         icon: 'success',
       }).then(() => {
-        window.localStorage.setItem("a_v_220725", "true")
+        window.localStorage.setItem("a_v_220728", "true")
       })
     }
   }, [announceViewed])
--- src/data/config.js
+++ src/data/config.js
@@ -1,10 +1,10 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "2.13.70"
+export const JAP_VER = "2.13.81"
 export const GLO_VER = "2.13.100"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101050131", "101060151"]                // 22.07.07
+export const JAP_NEW = ["101000311", "101040021"]                // 22.07.28
 export const GLO_NEW = ["101010111"]   // 22.07.25
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
