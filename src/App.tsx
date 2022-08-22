3be6ca3b5975e41ba7965cbe7562b40a2ac46630
Hyunuk Lee <as0266@naver.com>
2022-08-23 00:30:54 +0900

    220823 - 이프AS

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,34 @@ import ManifestPage from './components/pages/ManifestPage';
 import { GLO_VER, JAP_VER } from './data/config';
 
 const announceHTML = `<div class="announce">
-  일본판 ${JAP_VER}, 글로벌판 ${GLO_VER} 반영
+  글로벌판 AS이프 반영
   <br/>
-  Update JAP Ver.${JAP_VER}, GLO Ver.${GLO_VER}
+  Update Another Style Juno
   <br/>
   <br/>
   그 외 자세한 사항은 링크를 확인해 주세요. 
   <br/>
   Please Check following link.
   <br/>
-  <a href="https://aecheck.tistory.com/20" target="_blank" rel="noreferrer">Patch Note</a>
+  <a href="https://aecheck.tistory.com/21" target="_blank" rel="noreferrer">Patch Note</a>
 </div>`
 
 function App() {
 
-  const announceViewed = Boolean(window.localStorage.getItem("a_v_220811"))
+  const announceViewed = Boolean(window.localStorage.getItem("a_v_220823"))
 
   const { lang } = useContext(AnotherContext)
   const message = require(`./language/${lang}.json`)
 
   useEffect(() => {
-    window.localStorage.removeItem("a_v_220728")
+    window.localStorage.removeItem("a_v_220811")
     if(!announceViewed) {
       Swal.fire({
-        title: 'Update - 22.08.11',
+        title: 'Update - 22.08.23',
         html: announceHTML,
         icon: 'success',
       }).then(() => {
-        window.localStorage.setItem("a_v_220811", "true")
+        window.localStorage.setItem("a_v_220823", "true")
       })
     }
   }, [announceViewed])
