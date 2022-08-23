8e080a8c3802cdaf7dc39b068fe518b445ac2866
Hyunuk Lee <as0266@naver.com>
2022-08-24 00:45:22 +0900

    change config data

--- src/data/config.js
+++ src/data/config.js
@@ -5,9 +5,9 @@ export const GLO_VER = "2.13.200"
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
 export const JAP_NEW = ["101050081"]                // 22.08.09
-export const GLO_NEW = ["101000301", "101020011"]                // 22.08.11
+export const GLO_NEW = ["101070101"]                // 22.08.23
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
 export const JAP_MANIFEST_NEW = [189]
-export const GLO_MANIFEST_NEW = [113]
+export const GLO_MANIFEST_NEW = [17,168]
