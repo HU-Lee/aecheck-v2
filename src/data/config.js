5e5abcb1c0c52a7e3ff0047bf31e5a930de17a56
Hyunuk Lee <as0266@naver.com>
2022-11-10 15:22:47 +0900

    fix global version

--- src/data/config.js
+++ src/data/config.js
@@ -1,6 +1,6 @@
 // 현재 버전을 저장하는 string 값입니다.
 export const JAP_VER = "3.0.0"
-export const GLO_VER = "2.13.700"
+export const GLO_VER = "2.14.100"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
