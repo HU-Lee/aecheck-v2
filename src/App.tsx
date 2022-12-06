d7c5218cff7a05f3a619f451681d2fb51769a991
Hyunuk Lee <as0266@naver.com>
2022-12-06 22:12:12 +0900

    221206 GLO 2.14.2, JAP 3.0.20

--- src/App.tsx
+++ src/App.tsx
@@ -14,35 +14,35 @@ import { GLO_VER, JAP_VER } from './data/config';
 import BuddyPage from './components/pages/BuddyPage';
 
 const announceHTML = `<div class="announce">
-  일본판 ${JAP_VER} 반영
+  일본판 ${JAP_VER}, 글로벌판 ${GLO_VER} 반영
   <br/>
-  Update JAPANESE Ver.${JAP_VER}
+  Update JAPANESE Ver.${JAP_VER}, GLOBAL Ver.${GLO_VER}
   <br/>
   <br/>
   그 외 자세한 사항은 링크를 확인해 주세요. 
   <br/>
   Please Check following link.
   <br/>
-  <a href="https://aecheck.tistory.com/32" target="_blank" rel="noreferrer">Patch Note</a>
+  <a href="https://aecheck.tistory.com/33" target="_blank" rel="noreferrer">Patch Note</a>
 </div>`
 
 function App() {
 
-  const announceViewed = Boolean(window.localStorage.getItem("a_v_221125"))
+  const announceViewed = Boolean(window.localStorage.getItem("a_v_221206"))
 
   const { lang } = useContext(AnotherContext)
   const message = require(`./language/${lang}.json`)
 
   useEffect(() => {
-    window.localStorage.removeItem("a_v_221121")
+    window.localStorage.removeItem("a_v_221125")
     if(!announceViewed) {
     // if(true) {
       Swal.fire({
-        title: 'Update 22.11.25',
+        title: 'Update 22.12.06',
         html: announceHTML,
         icon: 'info',
       }).then(() => {
-        window.localStorage.setItem("a_v_221125", "true")
+        window.localStorage.setItem("a_v_221206", "true")
       })
     }
   }, [])
--- src/data/config.js
+++ src/data/config.js
@@ -1,11 +1,11 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "3.0.10"
-export const GLO_VER = "2.14.100"
+export const JAP_VER = "3.0.20"
+export const GLO_VER = "2.14.200"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101040161"]                // 22.11.25
-export const GLO_NEW = ["101010101"]                // 22.11.21
+export const JAP_NEW = ["101020121"]                // 22.12.06
+export const GLO_NEW = ["101070081"]                // 22.12.06
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
@@ -14,5 +14,5 @@ export const GLO_BUDDY_NEW = []                // 22.10.21
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
-export const JAP_MANIFEST_NEW = [211]
-export const GLO_MANIFEST_NEW = [189]
+export const JAP_MANIFEST_NEW = [199]
+export const GLO_MANIFEST_NEW = [147]
