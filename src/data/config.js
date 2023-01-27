faac78237b1a1e849c626904b49a0465396fb3b4
Lee Hyunuk <as0266@naver.com>
2023-01-27 20:13:20 +0900

    글판 2.14.510, 일판 3.0.70

--- src/data/config.js
+++ src/data/config.js
@@ -1,11 +1,11 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "3.0.60"
-export const GLO_VER = "2.14.500"
+export const JAP_VER = "3.0.70"
+export const GLO_VER = "2.14.510"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101040191"] // 23.1.12
-export const GLO_NEW = ["101050161", "101040181"] // 23.1.11
+export const JAP_NEW = ["101060101"] // 23.1.27
+export const GLO_NEW = ["101000321"] // 23.1.27
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
@@ -14,5 +14,5 @@ export const GLO_BUDDY_NEW = []                // 22.10.21
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
-export const JAP_MANIFEST_NEW = [100]
+export const JAP_MANIFEST_NEW = [6]
 export const GLO_MANIFEST_NEW = [112]
