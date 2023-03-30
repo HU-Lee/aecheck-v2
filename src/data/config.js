24d787d2766f4757cbcc183cab69ace663633183
Lee Hyunuk <as0266@naver.com>
2023-03-30 22:03:10 +0900

    lovely AS

--- src/data/config.js
+++ src/data/config.js
@@ -1,10 +1,10 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "3.1.10"
+export const JAP_VER = "3.1.20"
 export const GLO_VER = "3.0.110"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101010151", "101060081"] // 23.3.17
+export const JAP_NEW = ["101060121"] // 23.3.30
 export const GLO_NEW = ["101020121"] // 23.3.10
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
@@ -14,5 +14,5 @@ export const GLO_BUDDY_NEW = ["2000000001", "2000000002", "2000000003", "2000000
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
-export const JAP_MANIFEST_NEW = [6]
+export const JAP_MANIFEST_NEW = [188]
 export const GLO_MANIFEST_NEW = [199]
