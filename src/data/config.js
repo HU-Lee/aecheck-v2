610a11be41136e4c31d05096f467d6d7a3f4764b
Lee Hyunuk <as0266@naver.com>
2023-04-12 22:34:53 +0900

    jap 3.1.50

--- src/data/config.js
+++ src/data/config.js
@@ -1,10 +1,10 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "3.1.20"
+export const JAP_VER = "3.1.50"
 export const GLO_VER = "3.0.500"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101060121"] // 23.3.30
+export const JAP_NEW = ["101020161", "101000051", "101050161"] // 23.4.12
 export const GLO_NEW = ["101000031"] // 23.4.11
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
