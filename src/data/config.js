c2965564763bea720ad568d2169e62984e6ff722
Lee Hyunuk <as0266@naver.com>
2023-04-25 23:41:48 +0900

    이시층 호오즈키

--- src/data/config.js
+++ src/data/config.js
@@ -5,7 +5,7 @@ export const GLO_VER = "3.0.500"
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
 export const JAP_NEW = ["101020161", "101000051", "101050161"] // 23.4.12
-export const GLO_NEW = ["101000031"] // 23.4.11
+export const GLO_NEW = ["101040191"] // 23.4.25
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
@@ -15,4 +15,4 @@ export const GLO_BUDDY_NEW = ["2000000001", "2000000002", "2000000003", "2000000
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
 export const JAP_MANIFEST_NEW = [188]
-export const GLO_MANIFEST_NEW = [199]
+export const GLO_MANIFEST_NEW = [100]
