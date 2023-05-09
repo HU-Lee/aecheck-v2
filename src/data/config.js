1a255fdfe69bb670f066b7c02d45b884f17d8885
Lee Hyunuk <as0266@naver.com>
2023-05-09 23:03:02 +0900

    듀이 AS

--- src/data/config.js
+++ src/data/config.js
@@ -5,7 +5,7 @@ export const GLO_VER = "3.0.500"
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
 export const JAP_NEW = ["101050141"] // 23.4.28
-export const GLO_NEW = ["101040191"] // 23.4.25
+export const GLO_NEW = ["101060101"] // 23.5.09
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
@@ -15,4 +15,4 @@ export const GLO_BUDDY_NEW = ["2000000001", "2000000002", "2000000003", "2000000
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
 export const JAP_MANIFEST_NEW = [188]
-export const GLO_MANIFEST_NEW = [100]
+export const GLO_MANIFEST_NEW = [6]
