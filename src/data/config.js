8b6eb23717924c2cf0ebd5f3bc861263bf4e53b8
Lee Hyunuk <as0266@naver.com>
2023-05-15 22:07:28 +0900

    Ilulu alter

--- src/data/config.js
+++ src/data/config.js
@@ -1,15 +1,15 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "3.1.60"
+export const JAP_VER = "3.1.71"
 export const GLO_VER = "3.0.500"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101050141"] // 23.4.28
+export const JAP_NEW = ["101030181"] // 23.5.15
 export const GLO_NEW = ["101060101"] // 23.5.09
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_BUDDY_NEW = ["2000000007"]        // 23.4.28
+export const JAP_BUDDY_NEW = ["2000000008"]        // 23.5.15
 export const GLO_BUDDY_NEW = ["2000000001", "2000000002", "2000000003", "2000000004", "2000000005"]        // 23.2.24
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
