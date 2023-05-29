5fbf837b40ee6997593eb4eb285c18e4ff46e636
Lee Hyunuk <as0266@naver.com>
2023-05-29 21:36:54 +0900

    Premaya AS

--- src/data/config.js
+++ src/data/config.js
@@ -1,10 +1,10 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "3.1.71"
+export const JAP_VER = "3.1.80"
 export const GLO_VER = "3.0.500"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101030181"] // 23.5.15
+export const JAP_NEW = ["101050111"] // 23.5.15
 export const GLO_NEW = ["101060101"] // 23.5.09
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
