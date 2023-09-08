36450f5e83728c22ed292af35c4a4592136ab536
Lee Hyunuk <as0266@naver.com>
2023-09-09 01:02:18 +0900

    230909

--- src/data/config.js
+++ src/data/config.js
@@ -4,7 +4,7 @@ export const GLO_VER = "3.2.100"
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101000341", "101070131"] // 23.8.05
+export const JAP_NEW = ["101000361"] // 23.9.09
 export const GLO_NEW = ["101060091"] // 23.9.05
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
