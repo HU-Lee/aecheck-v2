d904e9690bddf657b8b563650caa03d77f35face
LHU <74810034+HU-Lee@users.noreply.github.com>
2023-11-07 12:32:50 +0900

    Update announcement

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { announceUpdate } from './data/constant';
 import { GLO_VER, JAP_VER } from './data/config';
 
 const announceHTML = `<div class="announce">
-  JP ${JAP_VER}, GLO ${GLO_VER} Update
+  GLO ${GLO_VER} Update
   <br/>
   <br/>
   근시일 내에 사이트가 변경될 예정입니다.<br/>
