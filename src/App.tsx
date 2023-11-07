f7c0ebd54e6c82700a941346730006dc74eda694
LHU <haulrest@gmail.com>
2023-11-07 19:16:55 +0900

    announce option 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { useContext, useEffect } from 'react';
-import { IntlProvider } from 'react-intl';
-import Swal from 'sweetalert2';
-import { AnotherContext } from './contexts';
+import React, { useContext, useEffect } from "react";
+import { IntlProvider } from "react-intl";
+import Swal from "sweetalert2";
+import { AnotherContext } from "./contexts";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from './components/organisms/Sidebar';
-import MainPage from './components/pages/MainPage';
-import ResultPage from './components/pages/ResultPage';
-import BookPage from './components/pages/BookPage';
-import { BackTop } from 'antd';
-import PersonalPage from './components/pages/PersonalPage';
-import ManifestPage from './components/pages/ManifestPage';
-import BuddyPage from './components/pages/BuddyPage';
-import { announceUpdate } from './data/constant';
-import { GLO_VER, JAP_VER } from './data/config';
+import Sidebar from "./components/organisms/Sidebar";
+import MainPage from "./components/pages/MainPage";
+import ResultPage from "./components/pages/ResultPage";
+import BookPage from "./components/pages/BookPage";
+import { BackTop } from "antd";
+import PersonalPage from "./components/pages/PersonalPage";
+import ManifestPage from "./components/pages/ManifestPage";
+import BuddyPage from "./components/pages/BuddyPage";
+import { announceUpdate } from "./data/config";
+import { GLO_VER, JAP_VER } from "./data/config";
 
 const announceHTML = `<div class="announce">
   GLO ${GLO_VER} Update
@@ -30,30 +30,30 @@ const announceHTML = `<div class="announce">
   <br/>
   <a href="https://www.reddit.com/r/AnotherEdenGlobal/comments/17d469m/aecheckcom_will_be_changed_soon/?utm_source=share&utm_medium=web2x&context=3" target="_blank" rel="noreferrer">English(Reddit)</a>
 
-</div>`
+</div>`;
 
 function App() {
-
-  const { lang } = useContext(AnotherContext)
-  const message = require(`./language/${lang}.json`)
+  const { lang } = useContext(AnotherContext);
+  const message = require(`./language/${lang}.json`);
 
   useEffect(() => {
-    window.localStorage.removeItem("a_v_230428")
-    const checkDay = window.localStorage.getItem("AE_INFO")
-    if (checkDay !== announceUpdate) {
+    window.localStorage.removeItem("a_v_230428");
+    const checkDay = window.localStorage.getItem("AE_INFO");
+    const announceKey = `${announceUpdate}_${lang}`;
+    if (checkDay !== announceKey) {
       // if (true) {
       Swal.fire({
-        title: 'Data Update',
+        title: "Data Update",
         html: announceHTML,
-        icon: 'info',
+        icon: "info",
       }).then(() => {
-        window.localStorage.setItem("AE_INFO", announceUpdate)
-      })
+        window.localStorage.setItem("AE_INFO", announceKey);
+      });
     }
-  }, [])
+  }, []);
 
   return (
-    <IntlProvider messages={message} locale={lang} defaultLocale='ko'>
+    <IntlProvider messages={message} locale={lang} defaultLocale="ko">
       <Router basename={process.env.PUBLIC_URL}>
         <Sidebar />
         <Routes>
