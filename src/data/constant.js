7840ae463c765d73ba65da00d88e5ba9d727fdce
LHU <74810034+HU-Lee@users.noreply.github.com>
2023-11-07 12:34:15 +0900

    Update announceDate

--- src/data/constant.js
+++ src/data/constant.js
@@ -8,4 +8,4 @@ export const STYLES = ["4.5", "ns", "as", "es"]
 // 현현의 순서와 관련된 array로 현현 관련 component에 다양하게 활용될 수 있습니다.
 export const MANIFEST_STEPS = ["-", "현현", "진현현"]
 
-export const announceUpdate = "231026"
+export const announceUpdate = "231107"
