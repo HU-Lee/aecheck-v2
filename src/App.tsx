f0a4298b7d45987b45bbd54dc3d6916b705f221f
LHU <haulrest@gmail.com>
2023-11-09 22:19:04 +0900

    231109 update

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { announceUpdate } from "./data/config";
 import { GLO_VER, JAP_VER } from "./data/config";
 
 const announceHTML = `<div class="announce">
-  GLO ${GLO_VER} Update
+  JAP ${JAP_VER}, GLO ${GLO_VER} Update
   <br/>
   <br/>
   근시일 내에 사이트가 변경될 예정입니다.<br/>
