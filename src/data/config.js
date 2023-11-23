b6b28308dd0b7062d0eccc58aaec2da156b31cdb
LHU <haulrest@gmail.com>
2023-11-23 19:51:50 +0900

    fix japanese new manifest

--- src/data/config.js
+++ src/data/config.js
@@ -14,7 +14,7 @@ export const GLO_BUDDY_NEW = ["2000000009", "2000000010"];
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
-export const JAP_MANIFEST_NEW = [135];
+export const JAP_MANIFEST_NEW = [93];
 export const GLO_MANIFEST_NEW = [125];
 
 export const announceUpdate = "231123";
