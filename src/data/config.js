27cbf0b5c226ec5dd36988679590697e6747e434
LHU <haulrest@gmail.com>
2023-12-08 18:31:52 +0900

    231208 Patch

--- src/data/config.js
+++ src/data/config.js
@@ -1,11 +1,11 @@
 // 현재 버전을 저장하는 string 값입니다.
-export const JAP_VER = "3.4.20";
-export const GLO_VER = "3.3.100";
+export const JAP_VER = "3.5.0";
+export const GLO_VER = "3.3.110";
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
-export const JAP_NEW = ["101020081", "101040031"];
-export const GLO_NEW = ["101050181"];
+export const JAP_NEW = ["101050191", "101450051", "101450061", "101450071"];
+export const GLO_NEW = ["101020171", "101450051", "101450061", "101450071"];
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
@@ -17,4 +17,4 @@ export const GLO_BUDDY_NEW = ["2000000009", "2000000010"];
 export const JAP_MANIFEST_NEW = [93];
 export const GLO_MANIFEST_NEW = [125];
 
-export const announceUpdate = "231123";
+export const announceUpdate = "231208";
