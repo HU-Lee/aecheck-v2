7b7a6b2977bc8c8eaac0348dad7e74a8c9b0b200
LHU <haulrest@gmail.com>
2024-01-15 21:20:17 +0900

    240115 Thillelille ES (Global)

--- src/data/config.js
+++ src/data/config.js
@@ -1,20 +1,20 @@
 // 현재 버전을 저장하는 string 값입니다.
 export const JAP_VER = "3.5.20";
-export const GLO_VER = "3.5.10";
+export const GLO_VER = "3.5.15";
 
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
 export const JAP_NEW = ["101020181"];
-export const GLO_NEW = ["101050171"];
+export const GLO_NEW = ["101010121"];
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
 export const JAP_BUDDY_NEW = ["2000000012"];
-export const GLO_BUDDY_NEW = ["2000000009", "2000000010"];
+export const GLO_BUDDY_NEW = ["2000000012"];
 
 // 현재 버전의 신규 현현을 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 id로 입력해 주세요.
 export const JAP_MANIFEST_NEW = [93];
 export const GLO_MANIFEST_NEW = [93];
 
-export const announceUpdate = "240112";
+export const announceUpdate = "240115";
