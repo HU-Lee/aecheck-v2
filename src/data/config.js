37b6fd6b82e0e548764052b84dcb01ff304b4cdd
LHU <haulrest@gmail.com>
2024-01-22 19:57:41 +0900

    240122 Akane AC

--- src/data/config.js
+++ src/data/config.js
@@ -5,7 +5,7 @@ export const GLO_VER = "3.5.15";
 // 현재 버전의 신규 캐릭터를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
 export const JAP_NEW = ["101020181"];
-export const GLO_NEW = ["101010121"];
+export const GLO_NEW = ["101020181"];
 
 // 현재 버전의 신규 버디를 저장하는 array입니다.
 // 데이터 파일에서 이름을 찾아 코드로 입력해 주세요.
@@ -17,4 +17,4 @@ export const GLO_BUDDY_NEW = ["2000000012"];
 export const JAP_MANIFEST_NEW = [93];
 export const GLO_MANIFEST_NEW = [93];
 
-export const announceUpdate = "240115";
+export const announceUpdate = "240122";
